6586d69b46e32f3358fca392fe13a566036ac6ae
mmorton <mmorton@icanhascode.com>
2011-02-20 21:23:59 -0700

    reworked how comments were associated with ast nodes.

--- lib/docjs/patch.rb
+++ lib/docjs/patch.rb
@@ -19,32 +19,27 @@ module RKelly
   class Parser
     private
     def apply_comments(ast)
-      link_children(ast, ast.value)
+      link_children(ast, *ast.value)
 
-      ast_hash = Hash.new { |h,k| h[k] = [] }
-      (ast || []).each do |n|
-        print "line: #{n.line}, node: #{n}\n"
-        ast_hash[n.line] << n if n.line
-      end
-      max = ast_hash.keys.sort.last
+      comment_hash = Hash.new
+
       @comments.each do |comment|
-        comment.line.upto(max) do |line|
-          if ast_hash.key?(line)
-            ast_hash[line].each do |node|
-              print "node line: #{node.line}, comment line: #{comment.line}\n"
-              node.comments << comment
-            end
-            break
-          end
-        end
-      end if max
+        next unless comment.line
+        for_line = comment.line + comment.value.count("\n") + 1
+
+        comment_hash[for_line] = comment
+      end
+
+      (ast || []).each do |node|
+        node.comments << comment_hash[node.line] if comment_hash[node.line]
+      end
+
       ast
     end
 
     def link_children(parent, *children)
       children.each do |child|
-        next unless child.respond_to? :parent
-        child.parent = parent
+        child.parent = parent if child.respond_to? :parent
         link_children(child, child.value) if child.respond_to? :value
       end
     end
--- samples/extjs-simple.js
+++ samples/extjs-simple.js
@@ -9,5 +9,23 @@ otherFunctionCall('this is a test');
  * @class document class
  */
 DocJS.Document = Ext.extend(Object, {
+    /**
+     * A constructor.
+     * @constructor
+     * @param o
+     */
+    constructor: function(o)
+    {
 
+    },
+    /**
+     * A function.
+     * @param a
+     * @param b
+     * @param c
+     */
+    methodA: function(a, b, c)
+    {
+
+    }
 });
